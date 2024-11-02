37cbed6e7e8157808f4cff91b2755e3587d91e3e
Cameron East <25549143+eastcw@users.noreply.github.com>
2024-11-02 23:23:30 +0000

    refactored to take any number of strings to compare

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# A script for calculating the intersection of three strings
+# A script for calculating the intersection of strings
 
-This script takes three text inputs, cleans them to accept only the 26 letters in the English alphabet, and then outputs the letters that are common to all three inputs.
+This script takes a number of text inputs, cleans them to accept only the 26 letters in the English alphabet, and then outputs the letters that are common to all inputs.
 
 The easiest way to run it will be to copy the script into an online python interpreter [such as this one.](https://www.online-python.com/) Alternatively you can download the script and install python and then run it locally on your computer via the command line.
--- calculator.py
+++ calculator.py
@@ -1,33 +1,36 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def calculate(string1, string2, string3):
+def calculate(strings):
     result = ""
-    dict1 = count_letters(string1)
-    dict2 = count_letters(string2)
-    dict3 = count_letters(string3)
+    letterCounts = []
+    for i in range(0,len(strings)):
+        letterCounts.append(count_letters(strings[i]))
     
-# this loop takes the dictionaries and for each place (letter) adds the smallest number of the three ditionaries
-# (this will be the crossover) to the result string, then the string is returned
+# this loop takes the dictionaries and for each place (letter) adds the smallest number in that position of all the
+# dictionaries (this will be the intersection) to the result string, then the string is returned
     for i in alphabet:
-        if dict1[i] != 0 and dict2[i] != 0 and dict3[i] != 0:
-            numRequired = min(dict1[i], dict2[i], dict3[i])
-            for j in range(0,numRequired):
-                result += i
-
+        smallest = letterCounts[0][i]
+        # this loop calculates the smallest number for a given letter in all the inputs
+        for j in range(0,len(letterCounts)):
+            if letterCounts[j][i] < smallest:
+                smallest = letterCounts[j][i]
+        # this loop adds that number of the right letter to the output
+        for k in range(0,smallest):
+            result += i
     return result    
 
 def clean_input(string):
     # cleans the strings by removing non-letter characters and converting upper-case letters to lower-case
-    lowerString = string.lower()
+    string = string.lower()
     cleanString = ""
-    for i in lowerString:
+    for i in string:
         if i in alphabet:
                 cleanString += i
     return cleanString
 
 def count_letters(string):
     dict = {}
-# creates a doctionary with entries at zero for each letter in the alphabet
+# creates a dictionary with entries at zero for each letter in the alphabet
     for i in alphabet:
         dict[i] = 0
 # counts every letter in the input string and stores the count in the sictionary, then returns the dictionary
@@ -38,24 +41,28 @@ def count_letters(string):
 def main():
     print(
           "Welcome to the calculator!\n",
-          "This will take three inputs and tell you what letters are common to all three.\n",
-          "First, you will need to enter three lists of words.\n",
+          "First, I will ask you how many lists you want to compare.\n",
+          "Then, you can enter your lists of words. \n",
           "These can be bunched up words or comma seperated lists,",
           " but each list should be inputted as a single input when asked."
     )
     input("Press enter to continue...")
     
     while True :
-    
-        string1 = input("Please input your first list here, and then press enter: ")
-        string2 = input("Okay, now please input your next list, followed by enter: ")
-        string3 = input("And finally, the last list followed by enter: ")
-        
-        cleanString1 = clean_input(string1)
-        cleanString2 = clean_input(string2)
-        cleanString3 = clean_input(string3)
-        
-        result = calculate(cleanString1, cleanString2, cleanString3)
+        strings = []
+        numOfStrings = 0
+        while not (isinstance(numOfStrings,int) and numOfStrings > 1):
+            try:
+                numOfStrings = int(input("How many lists do you want to compare? Please enter a whole number: "))
+            except ValueError:
+                input("The input here must be a whole number please. Press enter to continue.")
+                continue
+            if isinstance(numOfStrings,int) and numOfStrings > 1:
+                break
+            input("This number needs to be a positive number of two or more please. Press enter to continue.")
+        for i in range(0,numOfStrings):
+            strings.append(clean_input(input("Please enter list " + str(i+1) + ": ")))
+        result = calculate(strings)
         if result == "":
             result = "There are no common letters in these strings!"
         
