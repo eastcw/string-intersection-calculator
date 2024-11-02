26bf3b9d83759b69f64ac74580671d5ddac4da05
Cameron East <25549143+eastcw@users.noreply.github.com>
2024-11-02 09:54:20 +0000

    refactored to make more efficient and structured

--- calculator.py
+++ calculator.py
@@ -1,5 +1,40 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+def calculate(string1, string2, string3):
+    result = ""
+    dict1 = count_letters(string1)
+    dict2 = count_letters(string2)
+    dict3 = count_letters(string3)
+    
+# this loop takes the dictionaries and for each place (letter) adds the smallest number of the three ditionaries
+# (this will be the crossover) to the result string, then the string is returned
+    for i in alphabet:
+        if dict1[i] != 0 and dict2[i] != 0 and dict3[i] != 0:
+            numRequired = min(dict1[i], dict2[i], dict3[i])
+            for j in range(0,numRequired):
+                result += i
+
+    return result    
+
+def clean_input(string):
+    # cleans the strings by removing non-letter characters and converting upper-case letters to lower-case
+    lowerString = string.lower()
+    cleanString = ""
+    for i in lowerString:
+        if i in alphabet:
+                cleanString += i
+    return cleanString
+
+def count_letters(string):
+    dict = {}
+# creates a doctionary with entries at zero for each letter in the alphabet
+    for i in alphabet:
+        dict[i] = 0
+# counts every letter in the input string and stores the count in the sictionary, then returns the dictionary
+    for i in string:
+        dict[i] += 1
+    return dict
+
 def main():
     print(
           "Welcome to the calculator!\n",
@@ -26,49 +61,6 @@ def main():
         
         print("\nThe result is:")
         print(result, "\n")
-
-def clean_input(string):
-    # cleans the strings by removing non-letter characters and converting upper-case letters to lower-case
-    lowerString = string.lower()
-    cleanString = ""
-    for i in lowerString:
-        for j in alphabet:
-            if i == j:
-                cleanString += i
-                break
-    return cleanString
-
-def calculate(string1, string2, string3):
-    result = ""
-    dict1 = {}
-    dict2 = {}
-    dict3 = {}
-
-# these three dictionaries count the number of each letter in each string, they start at 26 x zero with letter keys
-    for i in alphabet:
-        dict1[i] = 0
-        dict2[i] = 0
-        dict3[i] = 0
-
-# these for loops count the numbers of each letter and store the values in the dictionaries
-    for i in string1:
-        dict1[i] += 1
-
-    for i in string2:
-        dict2[i] += 1
-        
-    for i in string3:
-        dict3[i] += 1
-    
-# this loop takes the dictionaries and for each place (letter) adds the smallest number of the three ditionaries
-# (this will be the crossover) to the result string, then the string is returned
-    for i in alphabet:
-        if dict1[i] != 0 and dict2[i] != 0 and dict3[i] != 0:
-            numRequired = min(dict1[i], dict2[i], dict3[i])
-            for j in range(0,numRequired):
-                result += i
-
-    return result    
 
 if __name__ == "__main__":
     main()
